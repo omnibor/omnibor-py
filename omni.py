6fb19db4e7a9e0cd3cc3403d98b42e58cd8181ca
R Marion <36964079+RobMarion@users.noreply.github.com>
2023-04-21 17:12:02 -0700

    Add files via upload

--- omni.py
+++ omni.py
@@ -2,6 +2,8 @@ import ast
 import hashlib
 import importlib
 import os
+import glob
+import sys
 
 node_info_set = set()
 nodes_unavailable_set = set()
@@ -18,6 +20,16 @@ def get_sha1(filename):
 
 
 def get_dependencies(module_path):
+    if os.path.exists("./__pycache__"):
+        pyc_files = glob.glob("." + '/**/*.pyc')
+        for f in pyc_files:
+            gitoid = "gitoid:blob:sha1:" + get_sha1(f)
+            node_info_set.add("node: " + f + " " + gitoid)
+
+    get_sub_dependencies(module_path)
+    return node_info_set
+        
+def get_sub_dependencies(module_path):
     with open(module_path, 'r') as f:
         tree = ast.parse(f.read(), module_path)
     
@@ -32,7 +44,8 @@ def get_dependencies(module_path):
                     imp_name = importlib.import_module(module.name)
                     filename = os.path.basename(imp_name.__file__)
                     hash = get_sha1(imp_name.__file__) 
-                    node_info_set.add("leaf: " + filename + ": " + hash)
+                    gitoid = "gitoid:blob:sha1:"+hash
+                    node_info_set.add("leaf: " + filename + " " + gitoid)
                     # for sub modules
                     if imp_name.__file__.endswith('.py'):
                         return get_dependencies(imp_name.__file__)
@@ -53,7 +66,9 @@ def write_manifest(deps):
 
 def main():
     # use the 'entry point' to be analyzed
-    entry_node = input("Enter the entry node (a .py file): ")
+    if len(sys.argv) < 2:
+        exit("Enter starting node. Ex: python omni.py entrypoint.py")
+    entry_node = sys.argv[1]
     if not  os.path.exists(entry_node):
         exit("File does not exist. Exiting.")
         
