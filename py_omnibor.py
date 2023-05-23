da61aea6f7978a7940983366a14523fc48884a64
R Marion <36964079+RobMarion@users.noreply.github.com>
2023-05-23 15:19:59 -0700

    Add files via upload

--- py_omnibor.py
+++ py_omnibor.py
@@ -3,7 +3,6 @@
 #
 import inspect
 import os
-import sys
 import importlib.util
 import hashlib
 import argparse
@@ -31,7 +30,7 @@ def get_imported_modules(entry_point):
             imported_modules.add(module_name)
             try:
                 file_path = inspect.getfile(module)
-                if file_path.endswith('.py'):
+                if file_path.endswith('.py') or file_path.endswith('.pyd') or file_path.endswith('.pyo'):
                     with open(file_path, 'rb') as f:
                         content = f.read()
                         hash_object = hashlib.sha1(content)
@@ -39,16 +38,17 @@ def get_imported_modules(entry_point):
                         module_name += f' ({GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig})'
             except Exception:
                 pass
-            for name, obj in inspect.getmembers(module):
-                if inspect.ismodule(obj):
-                    #print(f' {GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig}')
-                    gitoid = obj
-                    if not obj.__name__.startswith('builtins'):
-                        gitoid = f' {GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig}'
-                    modules_to_process.append(obj)
-                    module_to_write.add(gitoid)
-                    
-    #return imported_modules
+            try:    
+                for name, obj in inspect.getmembers(module):
+                    if inspect.ismodule(obj):
+                        if obj.__name__.startswith('builtins'):
+                            continue
+                        modules_to_process.append(obj)
+                        module_to_write.add(f' {GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig}')
+                    elif file_path.endswith('.pyd') or file_path.endswith('.pyo'):
+                        module_to_write.add(f' {GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig}')
+            except Exception:
+                print("WARNING: A library may need to be pip installed")                
     return module_to_write
 
 
@@ -60,14 +60,40 @@ def write_manifest(deps):
     except:
         print(f"Could not open {OUT_FILE_NAME}")
 
+def experiment_write_to_pyc(deps, target_file):
+    '''This will write directly to the .pyc file.
+    Writing to the pyc file seems to not damage the file
+    but it is probably a bad practice as there is no guarantee 
+    this will work in all releases or future releases of python'''
+    subdirectory = './__pycache__'
+    if not os.path.exists(subdirectory):
+        print("Cannot write to pyc file because it is not found")
+        return
+        
+    for file in os.listdir(subdirectory):
+        file_prefix = os.path.splitext(target_file)[0]
+        if file.startswith(file_prefix) and file.endswith('.pyc'):
+            file_path = os.path.join(subdirectory, file)
+            print("File found at:", file_path)
+            with open(file_path, 'a') as file:
+                file.write("BEGIN OMNIBOR\n")
+                file.write(deps)
+                file.write("\nEND OMNIBOR")
+            break
+    else:
+        print("File not found.")
+
 
 def main():
     parser = argparse.ArgumentParser(description='Retrieve imported modules with file paths and SHA1 hashes.')
+    parser.add_argument('--append-manifest', action='store_true', help='Optional flag to append manifest')
     parser.add_argument('entry_point', type=str, help='The path to the entry point Python file.')
     args = parser.parse_args()
 
     modules = get_imported_modules(args.entry_point)
     write_manifest(modules)
+    if args.append_manifest:
+        experiment_write_to_pyc(str(modules), args.entry_point)
     print(f"Done. Check {OUT_FILE_NAME} for output")
 
 
