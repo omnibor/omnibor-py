fcd66e1acfacc963148b0ee499837c0d7db00592
R Marion <36964079+RobMarion@users.noreply.github.com>
2023-05-23 15:24:30 -0700

    Add files via upload

--- test_2.py
+++ test_2.py
@@ -1,6 +1,12 @@
+# for testing py_omnibor
+# REQUIRES a pip install of the requests library
+#     if you do not pip install, py_omnibor will note that something was not installed
+#     but otherwise works
+# does nothing useful
 import sys
 import random
 import hashlib
+import requests
 
 def main():
     args = sys.argv[1:]
