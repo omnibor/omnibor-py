db34049f2d123b72453cfeafd4120c4ad4e1ea07
R Marion <36964079+RobMarion@users.noreply.github.com>
2023-05-23 15:21:46 -0700

    Add files via upload

--- test_1.py
+++ test_1.py
@@ -1,4 +1,6 @@
-# this should result in an empty file
+# for testing py_omnibor
+# does nothing useful
+
 import csv
 import json
 import os 
@@ -7,7 +9,15 @@ import collections
 import re
 
 def main():
-    pass
+    text = "Hello, my email is example@example.com. Please contact me at that address."
+
+    # Define a regular expression pattern to match email addresses
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+    
+    # Use the findall() function to extract all email addresses from the text
+    email_addresses = re.findall(pattern, text)
+    for email in email_addresses:
+        print(email)
 
 if __name__ == '__main__':
     main()
