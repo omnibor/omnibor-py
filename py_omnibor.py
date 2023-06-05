dcee14bd5ef33626431aabfb54ff70add67b5be2
R Marion <36964079+RobMarion@users.noreply.github.com>
2023-06-05 16:57:31 -0700

    Add files via upload

--- py_omnibor.py
+++ py_omnibor.py
@@ -9,6 +9,8 @@ import argparse
 
 GITOID_PREFIX = "gitoid:blob:sha1:"
 OUT_FILE_NAME = "OmniBor.sha1"
+BEGIN_OMNIBOR = "BEGIN OMNIBOR==\n"
+END_OMNIBOR   = "\nEND OMNIBOR=="
 
 
 def get_imported_modules(entry_point):
@@ -25,6 +27,7 @@ def get_imported_modules(entry_point):
     modules_to_process = [entry_point_module]
     while modules_to_process:
         module = modules_to_process.pop()
+        #print(module)
         module_name = module.__name__
         if module_name not in imported_modules:
             imported_modules.add(module_name)
@@ -48,9 +51,31 @@ def get_imported_modules(entry_point):
                     elif file_path.endswith('.pyd') or file_path.endswith('.pyo'):
                         module_to_write.add(f' {GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig}')
             except Exception:
-                print("WARNING: A library may need to be pip installed")                
-    return module_to_write
+                print("WARNING: A library may need to be pip installed")    
+    # we have to catch the pyc file separately because it has marshalled code and imports nothing
+    pyc_file = get_pyc_file_name_hash(entry_point)
+    if pyc_file:
+        module_to_write.add(pyc_file)
+    return sorted(module_to_write)
+    
 
+def get_pyc_file_name_hash(target_file):
+    subdirectory = './__pycache__'
+    if not os.path.exists(subdirectory):
+        print("Cannot write to pyc file because it is not found")
+        return
+    for file in os.listdir(subdirectory):
+        file_prefix = os.path.splitext(target_file)[0]
+        if file.startswith(file_prefix) and file.endswith('.pyc'):
+            file_path = os.path.join(subdirectory, file)
+            #print(file_path)
+            with open(file_path, 'rb') as file:
+                content = file.read()
+                hash_object = hashlib.sha1(content)
+                hex_dig = hash_object.hexdigest()
+                break
+    return (f' {GITOID_PREFIX}{os.path.basename(file_path)}:{hex_dig}')
+                
 
 def write_manifest(deps):
     try:
@@ -60,11 +85,10 @@ def write_manifest(deps):
     except:
         print(f"Could not open {OUT_FILE_NAME}")
 
+
 def experiment_write_to_pyc(deps, target_file):
     '''This will write directly to the .pyc file.
-    Writing to the pyc file seems to not damage the file
-    but it is probably a bad practice as there is no guarantee 
-    this will work in all releases or future releases of python'''
+    Has the effect of changing the Sha1. See readme.md about writing to the pyc file. '''
     subdirectory = './__pycache__'
     if not os.path.exists(subdirectory):
         print("Cannot write to pyc file because it is not found")
@@ -74,15 +98,18 @@ def experiment_write_to_pyc(deps, target_file):
         file_prefix = os.path.splitext(target_file)[0]
         if file.startswith(file_prefix) and file.endswith('.pyc'):
             file_path = os.path.join(subdirectory, file)
-            print("File found at:", file_path)
-            with open(file_path, 'a') as file:
-                file.write("BEGIN OMNIBOR\n")
-                file.write(deps)
-                file.write("\nEND OMNIBOR")
-            break
-    else:
-        print("File not found.")
-
+            with open(file_path, 'r') as file:
+                content = file.read()
+                omnibor = content.find("BEGIN OMNIBOR==")
+            # if gitoids already written to file, do not overwrite    
+            if omnibor == -1:
+                with open(file_path, 'a') as file:
+                    file.write("BEGIN OMNIBOR==\n")
+                    file.write(deps.replace(',', '\n'))
+                    file.write("\nEND OMNIBOR==")
+                break
+        else:
+            print("File not found.")
 
 def main():
     parser = argparse.ArgumentParser(description='Retrieve imported modules with file paths and SHA1 hashes.')
